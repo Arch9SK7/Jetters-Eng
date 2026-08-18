69da7ae801793d8d3970f262418f67ecd7135983
Skyla Kikari <skyprincess3ds@hotmail.com>
2026-08-18 00:16:53 -0600

    Added in unused Scripting from Bomberman Tournament

--- build.bat
+++ build.bat
@@ -104,14 +104,18 @@ tools\lzss -evo build\EndCred_Tiles.bin
 tools\armips.exe build.asm -stat
 tools\Atlas jetters_eng.gba script\jetters_000.txt
 tools\Atlas jetters_eng.gba script\jetters_001.txt
+tools\Atlas jetters_eng.gba script\jetters_002.txt
+tools\Atlas jetters_eng.gba script\jetters_003.txt
 tools\Atlas jetters_eng.gba script\jetters_004.txt
 tools\Atlas jetters_eng.gba script\jetters_005.txt
 tools\Atlas jetters_eng.gba script\jetters_006.txt
+tools\Atlas jetters_eng.gba script\jetters_007.txt
 tools\Atlas jetters_eng.gba script\jetters_008.txt
+tools\Atlas jetters_eng.gba script\jetters_009.txt
+tools\Atlas jetters_eng.gba script\jetters_010.txt
 tools\Atlas jetters_eng.gba script\jetters_011.txt
 tools\Atlas jetters_eng.gba script\jetters_012.txt
 tools\Atlas jetters_eng.gba script\jetters_013.txt
-tools\Atlas jetters_eng.gba script\jetters_007.txt
 
 tools\flips --create --ips jetters.gba jetters_eng.gba jetters_eng.ips
 tools\flips --create --bps-linear jetters.gba jetters_eng.gba jetters_eng.bps
